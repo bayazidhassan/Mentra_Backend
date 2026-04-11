7a311efa8ea6617bbcb1acfd1c76e046bccfcdb8
bayazidhassan <bayazidhassan776@gmail.com>
2026-04-11 18:54:59 +0600

    fix cors order

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,15 @@ import express from 'express';
 import router from './routes';
 const app = express();
 
-app.use(express.json());
-app.use(cookieParser());
 app.use(
   cors({
     origin: ['http://localhost:3000', 'https://mentra-one.vercel.app'],
     credentials: true,
   }),
 );
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/v1', router);
 
