78a0ddd00dda96333718a55177ca25fd38220a82
bayazidhassan <bayazidhassan776@gmail.com>
2026-04-23 02:37:38 +0600

    notification routes

---
+++ src/app/modules/notification/notificaton_routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { authMiddleware } from '../../../middleware/authMiddleware';
+import { notificationController } from './notification_controller';
+
+const router = Router();
+
+router.use(authMiddleware);
+
+router.get('/', notificationController.getMyNotifications);
+router.get('/unread-count', notificationController.getUnreadCount);
+router.patch('/:id/read', notificationController.markAsRead);
+router.patch('/mark-all-read', notificationController.markAllAsRead);
+
+export default router;
