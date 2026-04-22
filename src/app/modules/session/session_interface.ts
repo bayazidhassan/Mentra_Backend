d8c77f15b760b525dfe92fff9156b386c3717333
bayazidhassan <bayazidhassan776@gmail.com>
2026-04-23 02:18:26 +0600

    session interface

--- src/app/modules/session/session_interface.ts
+++ src/app/modules/session/session_interface.ts
@@ -1,19 +1,21 @@
 import { Types } from 'mongoose';
 
-export type TSessionStatus =
-  | 'pending'
-  | 'confirmed'
-  | 'completed'
-  | 'cancelled';
+export type TSessionStatus = 'pending' | 'accepted' | 'completed' | 'cancelled';
 
 export type TSession = {
   learner: Types.ObjectId;
   mentor: Types.ObjectId;
+  roadmap?: Types.ObjectId;
   title: string;
   description?: string;
   scheduledAt: Date;
-  duration: number;
-  status: TSessionStatus;
+  durationMinutes: number;
+  price?: number;
+  paymentStatus?: 'unpaid' | 'paid';
   meetingLink?: string;
-  price: number;
+  status: TSessionStatus;
+  ratingByLearner?: number;
+  feedbackByLearner?: string;
+  ratingByMentor?: number;
+  feedbackByMentor?: string;
 };
