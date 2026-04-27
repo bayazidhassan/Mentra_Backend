49ece4a324fa91a271dea31ab5deaecbd19eba12
bayazidhassan <bayazidhassan776@gmail.com>
2026-04-28 00:11:47 +0600

    message model

---
+++ src/app/modules/message/message_model.ts
@@ -0,0 +1,38 @@
+import { Schema, model } from 'mongoose';
+import { TMessage } from './message_interface';
+
+const messageSchema = new Schema<TMessage>(
+  {
+    conversationId: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    senderId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    receiverId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    sessionId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Session',
+    },
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Message = model<TMessage>('Message', messageSchema);
