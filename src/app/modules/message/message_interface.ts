94c25df1fa8dc9c0bb11dd3b1a8f1d9f81d6908a
bayazidhassan <bayazidhassan776@gmail.com>
2026-04-28 00:10:13 +0600

    message interface

---
+++ src/app/modules/message/message_interface.ts
@@ -0,0 +1,10 @@
+import { Types } from 'mongoose';
+
+export type TMessage = {
+  conversationId: string;
+  senderId: Types.ObjectId;
+  receiverId: Types.ObjectId;
+  sessionId?: Types.ObjectId;
+  text: string;
+  isRead: boolean;
+};
